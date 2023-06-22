47506dd8fb8b48246a73c9f58412abc3bfc42938
AClolinta <AClolinta@gmail.com>
2023-06-22 08:56:43 +0000

    更改了命名Bug

---
+++ WebServer/utility/FileUtil.cpp
@@ -0,0 +1,51 @@
+/*
+ * @Author: AClolinta AClolinta@gmail.com
+ * @Date: 2023-06-21 13:36:56
+ * @LastEditors: AClolinta AClolinta@gmail.com
+ * @LastEditTime: 2023-06-21 13:45:42
+ * @FilePath: /ACWebserver/WebServer /utility/FileUtil.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "FileUtil.hpp"
+using namespace aclolinta::logger;
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/// @brief  用户提供缓冲区,以追加方式写入
+/// @param filename
+AppendFile::AppendFile(std::string filename) : fp_(fopen(filename.c_str(), "ae")) {
+
+    setbuffer(fp_, buffer_, sizeof buffer_);
+}
+AppendFile::~AppendFile() { fclose(fp_); }
+
+/// @brief 要写入的数据的指针（logline），每个数据项的大小（1字节），要写入的数据项的个数（len），以及要写入的文件指针（fp_）。
+/// @param logline
+/// @param len
+/// @return
+size_t AppendFile::write(const char* logline, size_t len) {
+    // 不使用文件锁定机制
+    return fwrite_unlocked(logline, 1, len, fp_);
+}
+
+void AppendFile::append(const char* logline, const size_t len){
+    size_t n_ = this->write(logline,len);
+    size_t remain_line = len - n_;
+    while(remain_line > 0){
+        size_t x_ = this->write(logline + n_, len);
+        if(x_ ==0 ){
+            int err = ferror(fp_);
+            if (err) fprintf(stderr, "AppendFile::append() failed !\n");
+            break;
+        }
+        n_ += x_;
+        remain_line = len - n_;
+    }
+}
+
+void AppendFile::flush() { fflush(fp_); }
---
+++ WebServer/utility/FileUtil.hpp
@@ -0,0 +1,38 @@
+/*
+ * @Author: AClolinta AClolinta@gmail.com
+ * @Date: 2023-06-21 13:27:05
+ * @LastEditors: AClolinta AClolinta@gmail.com
+ * @LastEditTime: 2023-06-21 13:38:04
+ * @FilePath: /ACWebserver/WebServer /utility/FileUtil.hpp
+ * @Description:
+ * FileUtil是最底层的文件类，封装了Log文件的打开、写入并在类析构的时候关闭文件，底层使用了标准IO，该append函数直接向文件写。
+ */
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#include "noncopyable.hpp"
+
+namespace aclolinta {
+namespace logger {
+class AppendFile : noncopyable {
+   public:
+    explicit AppendFile(std::string filename);
+    ~AppendFile();
+
+   public:
+    void append(const char *logline, const size_t len);
+    void flush();
+
+   private:
+    size_t write(const char *logline, size_t len);
+
+   private:
+    /* data */
+    FILE *fp_;
+    char buffer_[64 * 1024];
+};
+
+}  // namespace logger
+}  // namespace aclolinta
