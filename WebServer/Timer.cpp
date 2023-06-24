038cf2d9d9509e8923775eae09331d088e8d9bb5
AClolinta <AClolinta@gmail.com>
2023-06-24 10:55:11 +0000

    工具类

--- WebServer/Timer.cpp
+++ WebServer/Timer.cpp
@@ -2,7 +2,7 @@
  * @Author: AClolinta AClolinta@gmail.com
  * @Date: 2023-06-23 11:54:18
  * @LastEditors: AClolinta AClolinta@gmail.com
- * @LastEditTime: 2023-06-23 12:13:51
+ * @LastEditTime: 2023-06-24 10:55:07
  * @FilePath: /ACWebserver/WebServer/Timer.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -54,6 +54,7 @@ void TimerNode::clearReq() {
     SPHttpData.reset();
     this->setDeleted();
 }
+// 向HttpData增加一个计时器
 void TimerManager::addTimer(std::shared_ptr<HttpData> SPHttpData, int timeout) {
     SPTimerNode new_node(new TimerNode(SPHttpData, timeout));
     timerNodeQueue.push(new_node);
